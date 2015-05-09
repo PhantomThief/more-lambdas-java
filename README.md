b32d953f4d21bf098ef8a25b1805946a0d64ee58
w.vela <boywang004@hotmail.com>
2015-05-09 21:23:54 +0800

    add testcase.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-more-lambdas
+more-lambdas [![Build Status](https://travis-ci.org/PhantomThief/more-lambdas-java.svg?branch=master)](https://travis-ci.org/PhantomThief/more-lambdas-java)
 =======================
 
 Some useful lambda implements for Java 8.
@@ -12,3 +12,23 @@ Some useful lambda implements for Java 8.
     <version>0.1.0-SNAPSHOT</version>
 </dependency>
 ```
+
+## Usage
+
+advanced distinct
+```Java
+List<String> list = ....;
+List<String> result = list.stream() //
+	.filter(MorePredicates.distinctUsing(Object::hashCode)) // distinct using hashCode
+	.collect(Collectors.toList());
+```
+
+more collectors
+```Java
+Map<Integer, String> map = ...;
+map.entrySet().stream() //
+	... // some ops
+	.collect(MoreCollectors.toMap()); // no need to map key and value again if it's an entry stream.
+```
+
+also, there is simple HPPC support in MoreCollectors.
---
+++ src/test/java/com/github/phantomthief/test/MorePredicatesTest.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package com.github.phantomthief.test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import com.github.phantomthief.util.MorePredicates;
+
+/**
+ * @author w.vela
+ */
+public class MorePredicatesTest {
+
+    private static final class Bean {
+
+        private final String name;
+        private final String address;
+
+        /**
+         * @param name
+         * @param address
+         */
+        Bean(String name, String address) {
+            this.name = name;
+            this.address = address;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((address == null) ? 0 : address.hashCode());
+            result = prime * result + ((name == null) ? 0 : name.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (!(obj instanceof Bean)) {
+                return false;
+            }
+            Bean other = (Bean) obj;
+            if (address == null) {
+                if (other.address != null) {
+                    return false;
+                }
+            } else if (!address.equals(other.address)) {
+                return false;
+            }
+            if (name == null) {
+                if (other.name != null) {
+                    return false;
+                }
+            } else if (!name.equals(other.name)) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "Bean [name=" + name + ", address=" + address + "]";
+        }
+
+    }
+
+    @Test
+    public void testDistinctUsing() {
+        List<Bean> source = Arrays.asList(new Bean("name1", "address1"),
+                new Bean("name1", "address2"), new Bean("name3", "address2"));
+        List<Bean> target = source.stream() //
+                .filter(MorePredicates.distinctUsing(Bean::getName)) //
+                .collect(Collectors.toList());
+        Set<String> result = new HashSet<>();
+        for (Bean bean : target) {
+            assert(result.add(bean.getName()));
+        }
+    }
+}

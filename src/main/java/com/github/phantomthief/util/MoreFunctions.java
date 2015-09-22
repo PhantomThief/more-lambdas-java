1a6d36e557c4b184544ba416b35202ae7ca45427
w.vela <boywang004@hotmail.com>
2015-09-22 10:08:16 +0800

    add catching callable.

--- src/main/java/com/github/phantomthief/util/MoreFunctions.java
+++ src/main/java/com/github/phantomthief/util/MoreFunctions.java
@@ -3,6 +3,8 @@
  */
 package com.github.phantomthief.util;
 
+import java.util.concurrent.Callable;
+
 import com.google.common.base.Throwables;
 
 /**
@@ -13,6 +15,31 @@ public class MoreFunctions {
     public interface FunctionWithThrowable<T, R, X extends Throwable> {
 
         R apply(T t) throws X;
+    }
+
+    public static final <R> R catching(Callable<R> callable) {
+        return catching(callable, (Class<Throwable>) null);
+    }
+
+    @SafeVarargs
+    public static final <R> R catching(Callable<R> callable,
+            Class<? extends Throwable>... catchThrowables) {
+        try {
+            return callable.call();
+        } catch (Throwable e) {
+            if (catchThrowables != null) {
+                for (Class<? extends Throwable> throwable : catchThrowables) {
+                    if (throwable != null) {
+                        if (throwable.isInstance(e)) {
+                            continue;
+                        } else {
+                            throw Throwables.propagate(e);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
     }
 
     public static final <T, R> R catching(FunctionWithThrowable<T, R, Throwable> function, T t) {

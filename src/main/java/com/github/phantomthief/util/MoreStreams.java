8fada1abba311d1b14cac9793367b4e8cd5abea7
w.vela <boywang004@hotmail.com>
2015-11-24 17:01:00 +0800

    add toStream for iterator/iterable.

--- src/main/java/com/github/phantomthief/util/MoreStreams.java
+++ src/main/java/com/github/phantomthief/util/MoreStreams.java
@@ -3,8 +3,16 @@
  */
 package com.github.phantomthief.util;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * @author w.vela
@@ -29,5 +37,30 @@ public class MoreStreams {
         } else {
             return IntStream.rangeClosed(to, from).map(i -> to - i + from);
         }
+    }
+
+    public static <T> Stream<T> toStream(Iterator<T> iterator) {
+        checkNotNull(iterator);
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(iterator,
+                        (Spliterator.NONNULL | Spliterator.IMMUTABLE | Spliterator.ORDERED)),
+                false);
+    }
+
+    public static <T> Stream<T> toStream(Iterable<T> iterable) {
+        checkNotNull(iterable);
+        if (iterable instanceof Collection) {
+            // failfast
+            try {
+                Collection<T> collection = (Collection<T>) iterable;
+                return StreamSupport.stream(Spliterators.spliterator(collection, 0), false);
+            } catch (Throwable e) {
+                // do nothing
+            }
+        }
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(iterable.iterator(),
+                        (Spliterator.NONNULL | Spliterator.IMMUTABLE | Spliterator.ORDERED)),
+                false);
     }
 }

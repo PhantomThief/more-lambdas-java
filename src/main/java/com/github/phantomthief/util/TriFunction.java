853e0d837c4559b3cc885bd7ec23f5a83b419864
w.vela <boywang004@hotmail.com>
2016-01-18 19:49:54 +0800

    add tri function.

---
+++ src/main/java/com/github/phantomthief/util/TriFunction.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.github.phantomthief.util;
+
+/**
+ * @author w.vela
+ */
+@FunctionalInterface
+public interface TriFunction<T1, T2, T3, R> {
+
+    R apply(T1 t1, T2 t2, T3 t3);
+}

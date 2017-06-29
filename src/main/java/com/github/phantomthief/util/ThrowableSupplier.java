fdcc2a7405ea4e11a3525cd51fe636dd557018ea
w.vela <wangtianzhou@kuaishou.com>
2017-06-29 10:53:27 +0800

    add factory method for generic type ref.

--- src/main/java/com/github/phantomthief/util/ThrowableSupplier.java
+++ src/main/java/com/github/phantomthief/util/ThrowableSupplier.java
@@ -9,4 +9,8 @@ package com.github.phantomthief.util;
 public interface ThrowableSupplier<T, X extends Throwable> {
 
     T get() throws X;
+
+    static <T, X extends Throwable> ThrowableSupplier<T, X> cast(ThrowableSupplier<T, X> func) {
+        return func;
+    }
 }

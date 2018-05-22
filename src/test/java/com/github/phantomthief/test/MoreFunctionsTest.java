b97273a2fd030aa2e520356fce8d30792f9ccb53
w.vela <wangtianzhou@kuaishou.com>
2018-05-22 13:25:48 +0800

    [utils] add kv functions for map stream API.

--- src/test/java/com/github/phantomthief/test/MoreFunctionsTest.java
+++ src/test/java/com/github/phantomthief/test/MoreFunctionsTest.java
@@ -1,6 +1,9 @@
 package com.github.phantomthief.test;
 
 import static com.github.phantomthief.util.MoreFunctions.catching;
+import static com.github.phantomthief.util.MoreFunctions.consumerKv;
+import static com.github.phantomthief.util.MoreFunctions.filterKv;
+import static com.github.phantomthief.util.MoreFunctions.mapKv;
 import static com.github.phantomthief.util.MoreFunctions.runParallel;
 import static com.github.phantomthief.util.MoreFunctions.runWithThreadName;
 import static com.github.phantomthief.util.MoreFunctions.throwing;
@@ -10,12 +13,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
 import com.google.common.base.Supplier;
+import com.google.common.collect.ImmutableMap;
 
 /**
  * @author w.vela
@@ -61,5 +66,16 @@ class MoreFunctionsTest {
         runWithThreadName(it -> it + mySuffix, () -> { //
             assertTrue(Thread.currentThread().getName().endsWith(mySuffix));
         });
+    }
+
+    @Test
+    void testKv() {
+        Map<String, Integer> map = ImmutableMap.of("test", 1, "a1", 2);
+        map.entrySet().stream() //
+                .filter(filterKv((k, v) -> true)) //
+                .forEach(consumerKv((k, v) -> System.out.println(k + "==>" + v)));
+        map.entrySet().stream() //
+                .map(mapKv((k, v) -> v)) //
+                .forEach(System.out::println);
     }
 }

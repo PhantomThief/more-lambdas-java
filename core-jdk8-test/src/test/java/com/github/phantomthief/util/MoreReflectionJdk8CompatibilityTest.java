2583488b72f501b802aee17704168ad974c2a0b9
w.vela <wangtianzhou@kuaishou.com>
2019-08-01 13:01:53 +0800

    [project] add jdk8-test.

--- core-jdk8-test/src/test/java/com/github/phantomthief/util/MoreReflectionJdk8CompatibilityTest.java
+++ core-jdk8-test/src/test/java/com/github/phantomthief/util/MoreReflectionJdk8CompatibilityTest.java
@@ -4,6 +4,7 @@ import static com.github.phantomthief.util.MoreReflection.getStackTraceProvider;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -12,6 +13,10 @@ import org.junit.jupiter.api.Test;
  */
 class MoreReflectionJdk8CompatibilityTest {
 
+    /**
+     * 需要使用 JDK8 来测试
+     */
+    @Disabled
     @Test
     void test() {
         StackTraceProvider stackTraceProvider = getStackTraceProvider();
